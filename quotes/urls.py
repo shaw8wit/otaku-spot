51189a999986d3cff3aa9b38357fdf9267484b73
shaw8wit <shaw8wit@gmail.com>
2020-11-30 21:03:46 +0530

    change quotes

--- quotes/urls.py
+++ quotes/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('search', views.search, name='search'),
     path('login', views.login_view, name='login'),
-    path('display', views.display, name='display'),
+    path('display/<int:n>', views.display, name='display'),
     path('logout', views.logout_view, name='logout'),
     path('register', views.register, name='register'),
 ]
